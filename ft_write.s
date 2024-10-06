291e72f4789aac454035af4ec128067176fe1c1a
Domen Banfi <banfidomen@gmail.com>
2024-10-06 20:46:01 +0200

    Fixed retun value and errno setting for write function

--- ft_write.s
+++ ft_write.s
@@ -1,14 +1,21 @@
 ; ----------------------------------------------------------------------------------------
 ; Preforms functionallity of write. Runs on 64-bit Linux only.
 ; ----------------------------------------------------------------------------------------
-global    ft_write
+            global  ft_write
+            extern  __errno_location
 
-section   .text
+            section .text
 
-ft_write:       push rbp
-                mov  rbp, rsp
-                mov rax, $1
-                syscall
-end:            mov rsp, rbp
-                pop rbp
-                ret
+ft_write:   push    rbp
+            mov     rbp, rsp
+            mov     rax, $1
+            syscall
+            cmp     rax, -1
+            jg      end
+            neg     rax
+            mov     rdi, rax
+            call    __errno_location  wrt ..plt
+            mov     [rax], rdi
+end:        mov     rsp, rbp
+            pop     rbp
+            ret
