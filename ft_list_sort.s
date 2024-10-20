35152f80b6885185832614df823bef9a7614cc0e
Domen Banfi <banfidomen@gmail.com>
2024-10-20 17:44:23 +0200

    Minor improvment in ft_list_sort

--- ft_list_sort.s
+++ ft_list_sort.s
@@ -39,31 +39,35 @@ ft_list_sort:   push rbp
                 push r13
                 push r14
                 push r15
-                mov rbx, rdi; pointer to list start
-                mov r13, rdi; pointer to current element
-                mov r12, qword[rsi]
+                push rdi; pointer to list start
+                mov r13, qword[rdi]; pointer to current element
+                mov r12, rsi
 _loop:          cmp r13, 0x0
                 je _end
-                mov r14, rbx ;pointer to scanned element
-                mov r15, rbx ;next of prev elelemt(at the beginig pointer to first elemet)
+                mov r9,  qword[rsp]
+                mov r14, qword[r9] ;pointer to scanned element
+                mov r15, r9 ;next of prev elelemt(at the beginig pointer to first elemet)
+                mov rbx, qword[r13 + 8] ; copy next of current element, used to increase current element
+                mov qword[r13 + 8], 0x0
+                cmp r13, r14
+                je _skip_loop   ;if scanned element is equal to current element insert isn't needed
                 jmp _start_loop2
 _loop2:         add r14, 8
                 mov r15, r14
                 mov r14, qword[r14]       
-_start_loop2:   mov rcx, qword[r13 + 8] ; copy next of current element, used to increase current element 
-                cmp r13, r14
-                je _next_loop   ;if scanned element is equal to current element insert isn't needed
+_start_loop2:   cmp r14, 0x0
+                je _next_loop   ;if scanned element is equal to 0 end of the linked list
                 mov rdi, [r13]
                 mov rsi, [r14]
                 call r12 ; call compare functiom
                 cmp rax, 0x0
                 jg _loop2  ; when current element is greater than scanned element go to next scanned element
-                mov qword[r15], r13     ;insert current element
-                mov rdx, qword[r14 + 8] 
-                mov qword[r13 + 8], rdx
-_next_loop:     mov r13, rcx; ; increse current element
+_next_loop:     mov qword[r15], r13     ;insert current element
+                mov qword[r13 + 8], r14
+_skip_loop:     mov r13, rbx; ; increse current element
                 jmp _loop
-_end:           pop r15
+_end:           pop rdi
+                pop r15
                 pop r14
                 pop r13
                 pop r12
